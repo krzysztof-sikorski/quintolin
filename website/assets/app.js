017775e11ba61b93c0c9b7d9e5e217c044234865
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2023-12-31 23:23:21 +0100

    Add Bootstrap framework to assets

--- website/assets/app.js
+++ website/assets/app.js
@@ -1,9 +1,6 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css'
+// import Bootstrap framework
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+// import application styles
+import './styles/app.css'
