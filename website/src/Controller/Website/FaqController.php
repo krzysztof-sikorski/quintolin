98e2d67dc8e6ce450349032573739678eeab032f
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-01-01 11:29:47 +0100

    Create dummy controller for FAQ page

---
+++ website/src/Controller/Website/FaqController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Website;
+
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+#[Route('/faq', name: 'app_faq', methods: [Request::METHOD_GET])]
+#[Template(template: 'website/faq.html.twig')]
+final readonly class FaqController
+{
+    public function __invoke(): array
+    {
+        return [];
+    }
+}
