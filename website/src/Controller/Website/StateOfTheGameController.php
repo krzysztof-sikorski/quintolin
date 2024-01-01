264df6989f2c90e3526212d2a81ca4efbe63318e
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-01-01 11:30:29 +0100

    Create dummy controller for "State of the game" page

---
+++ website/src/Controller/Website/StateOfTheGameController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Website;
+
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+#[Route('/sotg/2015-07-17', name: 'app_state_of_the_game', methods: [Request::METHOD_GET])]
+#[Template(template: 'website/state_of_the_game.html.twig')]
+final readonly class StateOfTheGameController
+{
+    public function __invoke(): array
+    {
+        return [];
+    }
+}
