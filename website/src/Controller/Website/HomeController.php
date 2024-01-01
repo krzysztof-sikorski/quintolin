09f18b68e4cd084ffdb204d7f4d3413d8fe8dbcb
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-01-01 09:56:32 +0100

    Move home controller to separate namespace

--- website/src/Controller/Website/HomeController.php
+++ website/src/Controller/Website/HomeController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Controller\Website;
 
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Component\HttpFoundation\Request;
