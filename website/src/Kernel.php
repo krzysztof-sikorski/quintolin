c782603c1e82354b2c94c6404a485e89d446d9cc
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-01-17 01:18:41 +0100

    Override important PHP settings in application kernel

--- website/src/Kernel.php
+++ website/src/Kernel.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace App;
 
+use Override;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+use function date_default_timezone_set;
+use function error_reporting;
+
+use const E_ALL;
 
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    #[Override]
+    public function boot(): void
+    {
+        error_reporting(error_level: E_ALL);
+        date_default_timezone_set(timezoneId: 'UTC');
+
+        parent::boot();
+    }
 }
