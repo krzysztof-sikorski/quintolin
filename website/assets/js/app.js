8ab79d8f01ba70f6fcccfaf7d456e5a0b5a692ec
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-01-17 14:49:29 +0100

    Move stylesheet refs from JS code directly into base template

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -1,8 +1,5 @@
 // import third-party resources
 import '@popperjs/core'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import '@fontsource/freckle-face/index.min.css'
 
-// import website stylesheet
-import '../css/app.css'
+window.console.log('Application module loaded.');
