378ee801f30daff9a453b54cab781905d58eaf76
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-01-21 02:00:44 +0100

    Add `world` parameter to dummy leaderboard page

--- website/src/Controller/Website/LeaderboardController.php
+++ website/src/Controller/Website/LeaderboardController.php
@@ -13,29 +13,29 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[AsController]
-#[Route(path: '/rankings/{metric}', name: self::ROUTE_NAME, methods: [Request::METHOD_GET])]
+#[Route(path: '/rankings/{metric?}/{world?}', name: self::ROUTE_NAME, methods: [Request::METHOD_GET])]
 #[Template(template: 'website/leaderboard.html.twig')]
 final readonly class LeaderboardController
 {
     public const string ROUTE_NAME = 'app_website_leaderboard';
     private const string DEFAULT_METRIC = 'frags';
+    private const string DEFAULT_WORLD = 'playground';
 
     public function __construct(
         private UrlGeneratorInterface $urlGenerator,
     ) {}
 
-    public function __invoke(null | string $metric = null): array | RedirectResponse
+    public function __invoke(null | string $metric = null, null | string $world = null): array | RedirectResponse
     {
         if (null === $metric) {
-            $url = $this->urlGenerator->generate(
-                name: self::ROUTE_NAME,
-                parameters: ['metric' => self::DEFAULT_METRIC],
-            );
+            $parameters = ['metric' => $metric ?? self::DEFAULT_METRIC, 'world' => $world ?? self::DEFAULT_WORLD];
+            $url = $this->urlGenerator->generate(name: self::ROUTE_NAME, parameters: $parameters);
             return new RedirectResponse(url: $url, status: Response::HTTP_FOUND);
         }
 
         return [
             'metric' => $metric,
+            'world' => $world,
         ];
     }
 }
