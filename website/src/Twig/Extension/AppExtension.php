efb80fec3a5a7910eb8e6053e55581e73a775aaa
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-02-01 00:33:41 +0100

    Move Twig extension to dedicated namespace and tweak syntax a bit

--- website/src/Twig/Extension/AppExtension.php
+++ website/src/Twig/Extension/AppExtension.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace App\Twig;
+namespace App\Twig\Extension;
 
 use App\Service\VersionReader;
 use Override;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
-final class AppVersionExtension extends AbstractExtension
+final class AppExtension extends AbstractExtension
 {
     public function __construct(
         private VersionReader $versionReader,
@@ -19,11 +19,11 @@ final class AppVersionExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('app_version', [$this, 'getVersion']),
+            new TwigFunction('app_version', $this->getAppVersion(...)),
         ];
     }
 
-    public function getVersion(): string
+    public function getAppVersion(): string
     {
         return ($this->versionReader)();
     }
