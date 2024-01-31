3740c0a4dab1d58270f94f73056290c53db4198e
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2024-02-01 00:37:19 +0100

    Convert `app_clock` global var into `app_server_time` Twig function

--- website/src/Twig/Extension/AppExtension.php
+++ website/src/Twig/Extension/AppExtension.php
@@ -6,12 +6,14 @@ namespace App\Twig\Extension;
 
 use App\Service\VersionReader;
 use Override;
+use Psr\Clock\ClockInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 final class AppExtension extends AbstractExtension
 {
     public function __construct(
+        private ClockInterface $clock,
         private VersionReader $versionReader,
     ) {}
 
@@ -19,8 +21,14 @@ final class AppExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
+            new TwigFunction('app_server_time', $this->getServerTime(...)),
             new TwigFunction('app_version', $this->getAppVersion(...)),
         ];
+    }
+
+    public function getServerTime(): \DateTimeImmutable
+    {
+        return $this->clock->now();
     }
 
     public function getAppVersion(): string
